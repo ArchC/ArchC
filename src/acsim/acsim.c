cfce79b6f987b72172c2ea44776746c24fbff902
Maxiwell Garcia <maxiwell@gmail.com>
2010-08-10 20:40:19 +0000

    Static Compiled Simulator

--- src/acsim/acsim.c
+++ src/acsim/acsim.c
@@ -3447,7 +3447,7 @@ void CreateMakefile(){
 
   fprintf( output, "$(EXE): $(OBJS) %s\n",
            (strlen(SYSTEMC_PATH) > 2) ? "$(SYSTEMC)/lib-$(TARGET_ARCH)/libsystemc.a" : "");
-  fprintf( output, "\t$(CC) $(CFLAGS) $(INC_DIR) $(LIB_DIR) -o $@ $(OBJS) $(LIBS) 2>&1 | c++filt\n\n");
+  fprintf( output, "\t$(CC) $(CFLAGS) $(INC_DIR) $(LIB_DIR) -o $@ $(OBJS) $(LIBS) 2>&1 -Xlinker --allow-multiple-definition  | c++filt\n\n");
 
   COMMENT_MAKE("Copy from template if main.cpp not exist");
   fprintf(output, "main.cpp:\n");
