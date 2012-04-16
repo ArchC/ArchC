89b61c4d9c3df7d7fe8815c6026ab1a9d0094c3d
Rafael Auler <rafaelauler@gmail.com>
2012-04-16 21:05:47 +0000

    Small fix in boot script to support MacOSX libtoolize

--- boot.sh
+++ boot.sh
@@ -2,6 +2,11 @@
 test -d config || mkdir config
 autoheader
 aclocal
-libtoolize --force --copy
+glibtoolize --version &> /dev/null
+if [ $? -eq 0 ]; then
+    glibtoolize --force --copy
+else
+    libtoolize --force --copy
+fi
 automake --add-missing --copy
 autoconf
