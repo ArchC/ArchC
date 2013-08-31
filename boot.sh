9b7b47af4b4f5174f9a49d2a185c8a35aaf4aa3a
Ivan Sichmann Freitas <ivansichfreitas@gmail.com>
2013-08-31 16:45:03 -0300

    Fix glibtoolize error redirection
    In Ubuntu derived distros (or anywhere that dash is used as the default shell),
    the error redirection must be done differently. Fix provided by Luis Felipe
    Strano Moraes.

--- boot.sh
+++ boot.sh
@@ -2,7 +2,7 @@
 test -d config || mkdir config
 autoheader
 aclocal
-glibtoolize --version &> /dev/null
+glibtoolize --version > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     glibtoolize --force --copy
 else
