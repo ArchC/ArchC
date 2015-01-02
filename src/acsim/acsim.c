f98d5f9ea9949dbc7f5b5b5dd6780216024a7958
maxiwell <maxiwell@gmail.com>
2015-01-02 19:48:56 -0200

    [acsim -nt -g] Bugfix in trace model: when a instruction is canceled (e.g. ac_annul in arm model), the next instruction is not traced because of longjmp.

--- src/acsim/acsim.c
+++ src/acsim/acsim.c
@@ -3689,140 +3689,143 @@ void EmitInstrExecIni( FILE *output, int base_indent) {
   \brief Used by EmitProcessorBhv function */
 /***************************************/
 void EmitInstrExec( FILE *output, int base_indent){
-  extern ac_dec_instr *instr_list;
-  extern ac_dec_format *format_ins_list;
-  extern char* project_name;
+    extern ac_dec_instr *instr_list;
+    extern ac_dec_format *format_ins_list;
+    extern char* project_name;
 
-  ac_dec_format *pformat;
-  ac_dec_instr *pinstr;
-  ac_dec_field *pfield;
+    ac_dec_format *pformat;
+    ac_dec_instr *pinstr;
+    ac_dec_field *pfield;
 
-  if( ACThreading ) {
-    fprintf(output, "%sI_Init:\n", INDENT[base_indent]);
-    fprintf(output, "%sgoto *dispatch();\n\n", INDENT[base_indent + 1]);
-    
-    if ( ACABIFlag && ACDecCacheFlag ) {
-      fprintf( output, "%s#define AC_SYSC(NAME,LOCATION) \\\n", 
-             INDENT[base_indent]);
-      fprintf( output, "%sSys_##LOCATION: \\\n", INDENT[base_indent]);
-      base_indent++;
-      
-      if( ACStatsFlag ){
-        fprintf( output, "%sISA.stats[%s_stat_ids::SYSCALLS]++; \\\n", 
-                INDENT[base_indent], project_name);
-      }
-
-      if( ACDebugFlag ){
-        fprintf( output, "%sif( ac_do_trace != 0 )\\\n", INDENT[base_indent]);
-        fprintf( output, "%strace_file << hex << ac_pc << dec << endl; \\\n", 
-                INDENT[base_indent + 1]);
-      }
-
-      fprintf( output, "%sISA.syscall.NAME(); \\\n", INDENT[base_indent]);
-      fprintf( output, "%sgoto *dispatch();\n\n", INDENT[base_indent]);
-      base_indent--;
-      
-      fprintf( output, "%s#include <ac_syscall.def>\n", INDENT[base_indent]);
-      fprintf( output, "%s#undef AC_SYSC\n\n", INDENT[base_indent]);
-    }
-  }
-  else {
-    EmitInstrExecIni( output, base_indent );
-  
-    /* Switch statement for instruction selection */
-    fprintf(output, "%sswitch (ins_id) {\n", INDENT[base_indent]);
-  }
-  
-  for (pinstr = instr_list; pinstr != NULL; pinstr = pinstr->next) {
     if( ACThreading ) {
-      fprintf(output, "%sI_%s: // Instruction %s\n", 
-            INDENT[base_indent], pinstr->name, pinstr->name);
-      if ( ACABIFlag ) {
-        fprintf(output, "%sISA._behavior_instruction(", INDENT[base_indent + 1]);
-        /* common_instr_field_list has the list of fields for the generic instruction. */
-        if( ACDecCacheFlag ){
-          for (pformat = format_ins_list;
-              (pformat != NULL) && strcmp(pinstr->format, pformat->name);
-              pformat = pformat->next);
-          for( pfield = common_instr_field_list; 
-               pfield != NULL; pfield = pfield->next) {
-            fprintf(output, "instr_dec->F_%s.%s", pformat->name, pfield->name);
-            if (pfield->next != NULL)
-              fprintf(output, ", ");
-          }
+        fprintf(output, "%sI_Init:\n", INDENT[base_indent]);
+        fprintf(output, "%sgoto *dispatch();\n\n", INDENT[base_indent + 1]);
+
+        if ( ACABIFlag && ACDecCacheFlag ) {
+            fprintf( output, "%s#define AC_SYSC(NAME,LOCATION) \\\n", 
+                    INDENT[base_indent]);
+            fprintf( output, "%sSys_##LOCATION: \\\n", INDENT[base_indent]);
+            base_indent++;
+
+            if( ACStatsFlag ){
+                fprintf( output, "%sISA.stats[%s_stat_ids::SYSCALLS]++; \\\n", 
+                        INDENT[base_indent], project_name);
+            }
+
+            if( ACDebugFlag ){
+                fprintf( output, "%sif( ac_do_trace != 0 )\\\n", INDENT[base_indent]);
+                fprintf( output, "%strace_file << hex << ac_pc << dec << endl; \\\n", 
+                        INDENT[base_indent + 1]);
+            }
+
+            fprintf( output, "%sISA.syscall.NAME(); \\\n", INDENT[base_indent]);
+            fprintf( output, "%sgoto *dispatch();\n\n", INDENT[base_indent]);
+            base_indent--;
+
+            fprintf( output, "%s#include <ac_syscall.def>\n", INDENT[base_indent]);
+            fprintf( output, "%s#undef AC_SYSC\n\n", INDENT[base_indent]);
         }
-        else {
-          for( pfield = common_instr_field_list; 
-              pfield != NULL; pfield = pfield->next){
-            fprintf(output, "ins_cache[%d]", pfield->id);
+    }
+    else {
+
+        if( ACDebugFlag ){
+            fprintf( output, "%sif( ac_do_trace != 0 ) \n", INDENT[base_indent]);
+            fprintf( output, PRINT_TRACE, INDENT[base_indent+1]);
+            fprintf( output, "\n");
+        }
+
+        EmitInstrExecIni( output, base_indent );
+
+        /* Switch statement for instruction selection */
+        fprintf(output, "%sswitch (ins_id) {\n", INDENT[base_indent]);
+    }
+
+    for (pinstr = instr_list; pinstr != NULL; pinstr = pinstr->next) {
+        if( ACThreading ) {
+            fprintf(output, "%sI_%s: // Instruction %s\n", 
+                    INDENT[base_indent], pinstr->name, pinstr->name);
+            if ( ACABIFlag ) {
+                fprintf(output, "%sISA._behavior_instruction(", INDENT[base_indent + 1]);
+                /* common_instr_field_list has the list of fields for the generic instruction. */
+                if( ACDecCacheFlag ){
+                    for (pformat = format_ins_list;
+                            (pformat != NULL) && strcmp(pinstr->format, pformat->name);
+                            pformat = pformat->next);
+                    for( pfield = common_instr_field_list; 
+                            pfield != NULL; pfield = pfield->next) {
+                        fprintf(output, "instr_dec->F_%s.%s", pformat->name, pfield->name);
+                        if (pfield->next != NULL)
+                            fprintf(output, ", ");
+                    }
+                }
+                else {
+                    for( pfield = common_instr_field_list; 
+                            pfield != NULL; pfield = pfield->next){
+                        fprintf(output, "ins_cache[%d]", pfield->id);
+                        if (pfield->next != NULL)
+                            fprintf(output, ", ");
+                    }
+                }
+                fprintf(output, ");\n");
+            }
+        }
+        else
+            /* opens case statement */
+            fprintf(output, "%scase %d: // Instruction %s\n", 
+                    INDENT[base_indent], pinstr->id, pinstr->name);
+
+        /* emits format behavior method call */
+        for (pformat = format_ins_list;
+                (pformat != NULL) && strcmp(pinstr->format, pformat->name);
+                pformat = pformat->next);
+        fprintf(output, "%sISA._behavior_%s_%s(", INDENT[base_indent + 1],
+                project_name, pformat->name);
+        for (pfield = pformat->fields; pfield != NULL; pfield = pfield->next) {
+            if( ACDecCacheFlag )
+                fprintf(output, "instr_dec->F_%s.%s", pformat->name, pfield->name);
+            else
+                fprintf(output, "ins_cache[%d]", pfield->id);
             if (pfield->next != NULL)
-              fprintf(output, ", ");
-          }
+                fprintf(output, ", ");
         }
         fprintf(output, ");\n");
-      }
-    }
-    else
-      /* opens case statement */
-      fprintf(output, "%scase %d: // Instruction %s\n", 
-              INDENT[base_indent], pinstr->id, pinstr->name);
-      
-    /* emits format behavior method call */
-    for (pformat = format_ins_list;
-         (pformat != NULL) && strcmp(pinstr->format, pformat->name);
-         pformat = pformat->next);
-    fprintf(output, "%sISA._behavior_%s_%s(", INDENT[base_indent + 1],
-            project_name, pformat->name);
-    for (pfield = pformat->fields; pfield != NULL; pfield = pfield->next) {
-      if( ACDecCacheFlag )
-        fprintf(output, "instr_dec->F_%s.%s", pformat->name, pfield->name);
-      else
-        fprintf(output, "ins_cache[%d]", pfield->id);
-      if (pfield->next != NULL)
-        fprintf(output, ", ");
-    }
-    fprintf(output, ");\n");
-    
-    /* emits instruction behavior method call */
-    fprintf(output, "%sISA.behavior_%s(", INDENT[base_indent + 1],
-            pinstr->name);
-    for (pfield = pformat->fields; pfield != NULL; pfield = pfield->next) {
-      if( ACDecCacheFlag )
-        fprintf(output, "instr_dec->F_%s.%s", pformat->name, pfield->name);
-      else
-        fprintf(output, "ins_cache[%d]", pfield->id);
-      if (pfield->next != NULL)
-        fprintf(output, ", ");
-    }
-    fprintf(output, ");\n");
-    
-    if( ACWaitFlag )
-      fprintf(output, "%sac_qk.inc(sc_time(module_period_ns*%d, SC_NS));\n", INDENT[base_indent + 1],
-              pinstr->cycles);
 
-    if( ACThreading )
-      fprintf(output, "%sgoto *dispatch();\n\n", INDENT[base_indent + 1]);
-    else
-      fprintf(output, "%sbreak;\n", INDENT[base_indent]);
-  }
-  
-  if( !ACThreading ) {
-    fprintf(output, "%s} // switch (ins_id)\n", INDENT[base_indent]);
+        /* emits instruction behavior method call */
+        fprintf(output, "%sISA.behavior_%s(", INDENT[base_indent + 1],
+                pinstr->name);
+        for (pfield = pformat->fields; pfield != NULL; pfield = pfield->next) {
+            if( ACDecCacheFlag )
+                fprintf(output, "instr_dec->F_%s.%s", pformat->name, pfield->name);
+            else
+                fprintf(output, "ins_cache[%d]", pfield->id);
+            if (pfield->next != NULL)
+                fprintf(output, ", ");
+        }
+        fprintf(output, ");\n");
 
-    if( ACStatsFlag ){
-      fprintf( output, "%sif(!ac_wait_sig) {\n", INDENT[base_indent]);
-      fprintf( output, "%sISA.stats[%s_stat_ids::INSTRUCTIONS]++;\n", 
-              INDENT[base_indent+1], project_name);
-      fprintf( output, "%s(*(ISA.instr_stats[ins_id]))[%s_instr_stat_ids::COUNT]++;\n", 
-              INDENT[base_indent+1], project_name);
-      fprintf( output, "%s}\n", INDENT[base_indent]);
+        if( ACWaitFlag )
+            fprintf(output, "%sac_qk.inc(sc_time(module_period_ns*%d, SC_NS));\n", INDENT[base_indent + 1],
+                    pinstr->cycles);
+
+        if( ACThreading )
+            fprintf(output, "%sgoto *dispatch();\n\n", INDENT[base_indent + 1]);
+        else
+            fprintf(output, "%sbreak;\n", INDENT[base_indent]);
     }
 
-    if( ACDebugFlag ){
-      fprintf( output, "%sif( ac_do_trace != 0 ) \n", INDENT[base_indent]);
-      fprintf( output, PRINT_TRACE, INDENT[base_indent+1]);
+    if( !ACThreading ) {
+        fprintf(output, "%s} // switch (ins_id)\n", INDENT[base_indent]);
+
+        if( ACStatsFlag ){
+            fprintf( output, "%sif(!ac_wait_sig) {\n", INDENT[base_indent]);
+            fprintf( output, "%sISA.stats[%s_stat_ids::INSTRUCTIONS]++;\n", 
+                    INDENT[base_indent+1], project_name);
+            fprintf( output, "%s(*(ISA.instr_stats[ins_id]))[%s_instr_stat_ids::COUNT]++;\n", 
+                    INDENT[base_indent+1], project_name);
+            fprintf( output, "%s}\n", INDENT[base_indent]);
+        }
+
     }
-  }
 }
 
 
